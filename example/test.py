9ddee0331f3d9b3a1761530f9d376fb34324a50e
qkrskdjf <qkrcksdjf1004@gmail.com>
2020-05-25 13:43:24 +0900

    파이썬 기초문법

--- example/test.py
+++ example/test.py
@@ -1,3 +1,19 @@
 num =123
 print(num)
 print(type(num))
+
+#인덱싱
+str1="Hello World!"
+print(str1[0])
+print(str1[1])
+
+
+#슬라이싱
+print(str1[0:3])
+print(str1[3:5])
+
+
+# 문자열 덧셈 곱셈
+str_num="123"
+print(str_num+str_num)
+print(str_num*3)
